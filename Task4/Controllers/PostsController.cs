6bf50f430034c11257fd4b3ddbe3aec87894a3d1
Alex <Aliabuharb8530@gmil.com>
2019-01-14 17:47:16 +0200

    init commit

---
+++ Task4/Controllers/PostsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task4.Models;
+
+namespace Task4.Controllers
+{ 
+    public class PostsController : Controller
+    {
+        private Post getPost(int id)
+        {
+
+            var post = Post.GetListPosts().SingleOrDefault(item  => item.Id ==id );
+
+            return post;
+        }
+
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View(Post.GetListPosts());
+        }
+
+        // GET: Posts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(getPost(id));
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create()
+        { 
+
+            
+            return View(new Post());
+        }
+
+        // POST: Posts/Create
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Post.AddNewPost(post);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(getPost(id));
+        }
+
+        // POST: Posts/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Post post)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Post.UpdatePost(post);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(post);
+            }
+        }
+
+        // GET: Posts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            
+            return View(getPost(id));
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Post post)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                if (Post.DeletePost(id))
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Content("Error , Can not deleted !!");
+
+                }
+
+            }
+            catch
+            {
+                return View(post);
+            }
+        }
+    }
+}
---
+++ Task4/Models/Post.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Task4.Models
+{
+
+
+
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime TimePosted { get; set; }
+        public string Content { get; set; }
+        public string  Owner { get; set; }
+        public string  Type { get; set; }
+
+
+        private  static List<Post> _Posts = new List<Post>
+        {
+
+            new Post
+            {
+                Id = 1,
+                Owner = "Alex Abu Harb",
+                TimePosted = DateTime.Now,
+                Title = "I Love Food",
+                Content = "Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                          "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+                          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" +
+                          " ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident," +
+                          " sunt in culpa qui officia deserunt mollit anim id est laborum consectetur " +
+                          "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna" +
+                          " aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris" +
+                          " nisi ut aliquip ex ea commodo consequat.",
+                Type = "Food"
+
+            },
+            new Post
+            {
+                Id = 2,
+                Owner = "Soma Somanooo",
+                TimePosted = DateTime.Now,
+                Title = "Blogging 2",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
+                          " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
+                          "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                          " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+                          "deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod " +
+                          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                          "quis nostrud " +"exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                          "consequat.",
+                Type = "Sport"
+            },
+            new Post
+            {
+                Id = 3,
+                Owner = "Ahmad  ",
+                TimePosted = DateTime.Now,
+                Title = "Blogging 3",
+                Content = " sed do eiusmod tempor" +
+                          " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
+                          "nostrud exercitation ullamc adipiscing elit, sed do eiusmod " +
+                          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                          "quis nostrud " +"exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                          "consequat.",
+                Type = "Playing"
+            }
+
+        };
+
+
+        public static List<Post> GetListPosts()
+        {
+            return _Posts;
+        }
+
+        public static void AddNewPost(Post post)
+        {
+            _Posts.Add(post);
+        }
+
+        public  static void  UpdatePost(Post post)
+        {
+
+            for (int i = 0; i < _Posts.Count; i++)
+            {
+                if (post.Id == _Posts[i].Id)
+                {
+                    _Posts[i] = post;
+                }
+            }
+        }
+
+        public static Boolean DeletePost(int  id)
+        {
+            bool b = false;
+
+            for (int i = 0; i < _Posts.Count; i++)
+            {
+                if (id == _Posts[i].Id)
+                {
+                    _Posts.RemoveAt(i);
+                    b = true;
+                }
+            }
+
+            return b;
+        }
+
+        
+    }
+}
